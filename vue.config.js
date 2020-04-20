a8125ac21e7ebb2586457764ce616c69deb152eb
youkang <13247323526@163.com>
2020-04-20 11:44:24 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
     publicPath: '/',
-    outputDir: 'mimi',
+    outputDir: 'test',
     lintOnSave: process.env.NODE_ENV !== 'production',
     devServer: {
         port: 3000,
         proxy: {
-            // 商户
-            '/merchant-admin': {
-                target: 'http://192.168.0.235:8081',
-                // target: 'https://merchant.jinzun666.com/',
+            '/': {
+                target: '',
                 ws: false,
                 changeOrigin: true
             }
@@ -16,7 +14,7 @@ module.exports = {
     },
     chainWebpack: (config) => {
         config.plugin('html').tap(args => {
-            args[0].title = '秘密接入'
+            args[0].title = 'test'
             return args
         })
         if(process.env.NODE_ENV === 'production') {
